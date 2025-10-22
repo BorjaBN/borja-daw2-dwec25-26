d7615f1680087d7bd4cacb5cd885a719f3143b55
DAW2 <fbdelacruzl01@educarex.es>
2025-10-22 16:07:45 +0200

    Añadiendo retos1 y modificaciones de estructura del repo

---
+++ retos/reto1.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let numeros = [1,2,3,4,5,6,7,8,9,10];
+
+function mostrarContenido(numeros){
+    for (let valores of numeros){
+        
+    }
+}
+mostrarContenido();
+console.log(numeros)
