2fb196123ec8bad790af8aabe0019e4a9e65bbcb
DAW2 <fbdelacruzl01@educarex.es>
2025-11-12 16:36:07 +0100

    Añadiendo reto2 y modificación de estructura de repo

---
+++ retos/reto2_numerosPares/MostrarPares.mjs
@@ -0,0 +1,27 @@
+
+//-----------------------------------------------------------------------
+// Inicialización
+//-----------------------------------------------------------------------
+
+$("body").ready(() => {
+    $("#botonMostrarPares").on("click", mostrarPares);
+    $("#botonLimpiar").on("click", () => $('textoResultado').val(''));
+});
+
+
+//-----------------------------------------------------------------------
+// Gestores de eventos
+//-----------------------------------------------------------------------
+
+function mostrarPares() {
+
+    $("textoResultado").val('');
+    const numero = $("numero").val();
+
+    for(let n = 1;n <= numero;n++) {
+        if(n % 2 == 0) {
+            $("textoResultado").val($("textoResultado").val()+','+n);
+        }
+    }
+
+}
